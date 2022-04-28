81386e7a1197cde8e084e05578dd91ae67c63ec6
Duy Dev <duy99.uit@gmail.com>
2022-04-28 12:49:20 +0700

    remove hepler folder

--- src/helpers/formatNumber/index.ts
+++
@@ -1,7 +0,0 @@
-import numbro from 'numbro';
-export const formatNumber = (param: number) => {
-    var numberFormatToString = numbro(param).format({
-        thousandSeparated: true
-    });
-    return numberFormatToString;
-}
